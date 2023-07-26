0fbe20fd403844b80899f81bd851bd4c0398091d
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-26 19:53:06 +0000

    completed second step of conversion

--- app.rb
+++ app.rb
@@ -3,11 +3,25 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
+raw_data= HTTP.get("https://api.exchangerate.host/symbols")
+parsed_data = JSON.parse(raw_data)
 
 get("/") do
-  @raw_data= HTTP.get("https://api.exchangerate.host/symbols")
-  @parsed_data = JSON.parse(@raw_data)
-  @symbols = @parsed_data.fetch("symbols").keys
-
+  @symbols = parsed_data.fetch("symbols").keys
+  
   erb(:home)
+end
+
+get("/:symbol") do
+  @symbol = params.fetch("symbol")
+
+  @symbols = parsed_data.fetch("symbols").keys
+
+  erb(:second_step)
+end
+
+get("/:symbol/:conversion") do
+  @symbol = params.fetch("symbol")
+  @conversion = params.fetch("conversion")
+  erb(:third_step)
 end
